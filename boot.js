d114b5b4c411a8b350744f806b7bb308a105e870
hocgin <hocgin@gmail.com>
2022-12-05 22:12:03 +0800

    umi4 ssr

---
+++ boot.js
@@ -0,0 +1,31 @@
+const express = require('express');
+let env = require("./config/env");
+console.log('服务配置', env);
+
+const app = express();
+const port = env.SERVER_PORT ?? 7000;
+const filepath = env.FILE_PATH ?? __dirname;
+
+app.use(async (req, res, next) => {
+    if (req.path.match(/([0-9]+|umi)\.js/)) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+    next();
+})
+
+// Logger middleware
+app.use((req, res, next) => {
+    console.info(`${req.method} ${req.url}`);
+    next();
+});
+
+// Umi SSR middleware
+app.use(require(filepath + '/umi.server.js').default);
+
+// Umi static files (including SSG pages)
+app.use(express.static(filepath));
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is now running at http://127.0.0.1:${port}/`);
+});
